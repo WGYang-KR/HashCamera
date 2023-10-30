b86e60e41a340e2b9dad7c28ae775a80d633d576
WG-MacHome <wgyang@WG-MacHome.local>
2023-10-30 23:30:41 +0900

    스토리지셀 구현

--- HashCamera/StorageBar/Components/StorageBarItemCell.swift
+++ HashCamera/StorageBar/Components/StorageBarItemCell.swift
@@ -11,6 +11,7 @@ import Photos
 
 class StorageBarItemCell: UICollectionViewCell {
     
+    ///스토리지 종류
     var storageType: StorageType = .photoLibrary
     
     ///스토리지 아이콘 이미지 표시뷰
@@ -19,6 +20,9 @@ class StorageBarItemCell: UICollectionViewCell {
         imageView.contentMode = .scaleAspectFill
         return imageView
     }()
+    
+    ///셀 선택 효과 뷰
+    private var selectionCoverView: UIView = makeCoverView()
     
     //MARK: - init
     override init(frame: CGRect) {
@@ -32,13 +36,26 @@ class StorageBarItemCell: UICollectionViewCell {
     }
     
     private func initView() {
+    
         self.addSubview(thumbnailImageView)
+        self.addSubview(selectionCoverView)
+        
+        self.backgroundColor = .white //하얀색 바탕
+        self.layer.cornerRadius = 3.0 //라운드 코너
+        self.clipsToBounds = true //자식뷰도 라운드 코너 적용
+
         
         thumbnailImageView.snp.makeConstraints { make in
+            make.edges.equalTo(self).inset(8) //상하좌우 여백 8
+        }
+        
+        selectionCoverView.snp.makeConstraints { make in
             make.edges.equalTo(self)
         }
         
+        updateUI(isSelected: false) //선택안된 UI로 초기화
     }
+
     //MARK: - configure
     
     func configure(storageType: StorageType) {
@@ -46,15 +63,44 @@ class StorageBarItemCell: UICollectionViewCell {
         self.thumbnailImageView.image = iconImage(storageType: storageType)
     }
     
+    //MARK: - selection
+    override var isSelected: Bool {
+        willSet {
+            updateUI(isSelected: isSelected)
+        }
+    }
+    
+    private func updateUI(isSelected: Bool) {
+            self.selectionCoverView.isHidden = !isSelected
+    }
+    
+    //MARK: -
     private func iconImage(storageType: StorageType) -> UIImage {
         switch storageType {
         case .iCloudDrive:
-            return UIImage(named: "iconiCloud")!
+            return UIImage(named: "tabIconiCloud")!
         case .localDrive:
-            return  UIImage(named: "iconFile")!
+            return  UIImage(named: "tabIconFile")!
         case .photoLibrary:
-            return UIImage(named: "iconPhotoLibrary")!
+            return UIImage(named: "tabIconPhotoLibrary")!
         }
+    }
+    
+    static private func makeCoverView() -> UIView {
+        let view = UIView()
+        view.backgroundColor = UIColor(white: 1.0, alpha: 0.5) //배경 불투명
+        
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "checkmark")?.withTintColor(.white)
+        imageView.contentMode = .scaleAspectFit
+        
+        view.addSubview(imageView)
+        imageView.snp.makeConstraints { make in
+            make.centerX.centerY.equalTo(view) //컨테이너의 중앙
+            make.width.equalTo(view).multipliedBy(0.25) // 컨테이너의 1/4
+        }
+        
+        return view
     }
     
 }
