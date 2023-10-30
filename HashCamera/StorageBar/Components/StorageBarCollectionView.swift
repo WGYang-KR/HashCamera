169422be2594f547f05cc18a32df0343ef060f16
WG-MacHome <wgyang@WG-MacHome.local>
2023-10-30 23:30:54 +0900

    스토리지 콜렉션뷰 구현중

--- HashCamera/StorageBar/Components/StorageBarCollectionView.swift
+++ HashCamera/StorageBar/Components/StorageBarCollectionView.swift
@@ -8,10 +8,26 @@
 import UIKit
 import SnapKit
 
-class StorageBarCollectionView: UICollectionView {
+class StorageBarCollectionView: UICollectionView,UICollectionViewDelegate, UICollectionViewDataSource {
+  
     
     let cellSize = CGSize(width: 60, height: 60)
+    var storageList: [StorageType] = [.iCloudDrive, .localDrive, .photoLibrary]
     
-    var storageList = [StorageData]()
+
+    func initView() {
+        self.delegate = self
+        self.dataSource = self
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return storageList.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "\(StorageBarItemCell.self)", for: indexPath) as? StorageBarItemCell else { return UICollectionViewCell() }
+        
+        return cell
+    }
     
 }
