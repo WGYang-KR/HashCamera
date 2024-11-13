9bdf5ebb43b551298bbedf9a227d2f146f19c3e7
WGYang-KR <anto.wg.yang@gmail.com>
2024-11-14 00:22:46 +0900

    폴더 이동 함수 내 변수명 변경

--- HashCamera/MoveToFolderScene/MoveToFolderVM.swift
+++ HashCamera/MoveToFolderScene/MoveToFolderVM.swift
@@ -33,42 +33,42 @@ class MoveToFolderVM: SelectableFolderListVM {
     
     ///2. 파일 이동을 진행하는 함수 (Result로 반환)
     func moveFiles(overwrite: Bool) async throws -> [Result<URL, FileMoveError>] {
-        guard let destination = selectedFolder?.url else { throw FileMoveError.unknown }
+        guard let destFolderURL = selectedFolder?.url else { throw FileMoveError.unknown }
         let files = targetFileList.map({ $0.url })
         
         var results = [Result<URL, FileMoveError>]() // 이동 결과를 저장하는 리스트
         
         for file in files {
-            let destinationFile = destination.appendingPathComponent(file.lastPathComponent)
+            let newFileURL = destFolderURL.appendingPathComponent(file.lastPathComponent)
             
             // 파일이 이미 존재할 경우 처리
-            if fileManager.fileExists(atPath: destinationFile.path) {
+            if fileManager.fileExists(atPath: newFileURL.path) {
                 if overwrite {
                     // 덮어쓰기
                     do {
-                        try fileManager.removeItem(at: destinationFile)
-                        try fileManager.moveItem(at: file, to: destinationFile)
-                        hcLog("Overwritten: \(destinationFile.lastPathComponent)")
-                        results.append(.success(destinationFile)) // 성공한 파일 추가
+                        try fileManager.removeItem(at: newFileURL)
+                        try fileManager.moveItem(at: file, to: newFileURL)
+                        hcLog("Overwritten: \(newFileURL.lastPathComponent)")
+                        results.append(.success(newFileURL)) // 성공한 파일 추가
                     } catch {
                         hcLog("Error overwriting \(file.lastPathComponent): \(error)")
                         results.append(.failure(.fileOverwriteFailed(file: file, error: error))) // 실패한 파일과 에러 추가
                     }
                 } else {
                     // 이름 변경
-                    var newDestinationFile = destinationFile
+                    var newNoExistFileURL = newFileURL
                     var count = 1
                     
-                    while fileManager.fileExists(atPath: newDestinationFile.path) {
-                        let newName = "\(destinationFile.deletingPathExtension().lastPathComponent) (\(count))"
-                        newDestinationFile = destination.appendingPathComponent(newName).appendingPathExtension(file.pathExtension)
+                    while fileManager.fileExists(atPath: newNoExistFileURL.path) {
+                        let newName = "\(newFileURL.deletingPathExtension().lastPathComponent) (\(count))"
+                        newNoExistFileURL = destFolderURL.appendingPathComponent(newName).appendingPathExtension(file.pathExtension)
                         count += 1
                     }
                     
                     do {
-                        try fileManager.moveItem(at: file, to: newDestinationFile)
-                        hcLog("Renamed and moved: \(newDestinationFile.lastPathComponent)")
-                        results.append(.success(newDestinationFile)) // 성공한 파일 추가
+                        try fileManager.moveItem(at: file, to: newNoExistFileURL)
+                        hcLog("Renamed and moved: \(newNoExistFileURL.lastPathComponent)")
+                        results.append(.success(newNoExistFileURL)) // 성공한 파일 추가
                     } catch {
                         hcLog("Error renaming \(file.lastPathComponent): \(error)")
                         results.append(.failure(.fileRenameFailed(file: file, error: error))) // 실패한 파일과 에러 추가
@@ -77,9 +77,9 @@ class MoveToFolderVM: SelectableFolderListVM {
             } else {
                 // 파일이 존재하지 않으면 그냥 이동
                 do {
-                    try fileManager.moveItem(at: file, to: destinationFile)
+                    try fileManager.moveItem(at: file, to: newFileURL)
                     hcLog("Moved: \(file.lastPathComponent)")
-                    results.append(.success(destinationFile)) // 성공한 파일 추가
+                    results.append(.success(newFileURL)) // 성공한 파일 추가
                 } catch {
                     hcLog("Error moving \(file.lastPathComponent): \(error)")
                     results.append(.failure(.fileMoveFailed(file: file, error: error))) // 실패한 파일과 에러 추가
