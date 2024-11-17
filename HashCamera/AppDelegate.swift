d1b19b2f071f0837c721b0f2d82840495466a583
WGYang-KR <anto.wg.yang@gmail.com>
2024-11-17 21:00:24 +0900

    파이어베이스 설치

--- HashCamera/AppDelegate.swift
+++ HashCamera/AppDelegate.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseAnalytics
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
+        initFirebase()
         return true
     }
 
@@ -29,6 +32,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+    func initFirebase() {
+        FirebaseApp.configure()
+        
+#if DEBUG
+        Analytics.setAnalyticsCollectionEnabled(false)
+#else
+        Analytics.setAnalyticsCollectionEnabled(true)
+#endif
+        
     }
 
 
