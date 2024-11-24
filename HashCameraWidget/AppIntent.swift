eb3332ec97828aca911ccd9dc3d0949fa23ba379
WGYang-KR <anto.wg.yang@gmail.com>
2024-11-24 20:41:03 +0900

    위젯 Extension 추가

---
+++ HashCameraWidget/AppIntent.swift
@@ -0,0 +1,18 @@
+//
+//  AppIntent.swift
+//  HashCameraWidget
+//
+//  Created by Anto-Yang on 11/24/24.
+//
+
+import WidgetKit
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource { "Configuration" }
+    static var description: IntentDescription { "This is an example widget." }
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
---
+++ HashCameraWidget/HashCameraWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  HashCameraWidgetBundle.swift
+//  HashCameraWidget
+//
+//  Created by Anto-Yang on 11/24/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct HashCameraWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        HashCameraWidget()
+        HashCameraWidgetControl()
+    }
+}
