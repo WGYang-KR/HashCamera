b47f2cde00d042bb06877c08ddfd2ca45b7ff9bb
WGYang-KR <anto.wg.yang@gmail.com>
2024-11-30 23:29:40 +0900

    위젯 화면 파일 생성

---
+++ HashCamera/WidgetSettingScene/WidgetSettingView.swift
@@ -0,0 +1,18 @@
+//
+//  WidgetSettingView.swift
+//  HashCamera
+//
+//  Created by Anto-Yang on 11/30/24.
+//
+
+import SwiftUI
+
+struct WidgetSettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WidgetSettingView()
+}
