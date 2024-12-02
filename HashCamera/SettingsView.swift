575654f10813e1b0cc4f4aadab9e48699b1221f1
WGYang-KR <anto.wg.yang@gmail.com>
2024-12-02 19:38:32 +0900

    네비게이션 설정

--- HashCamera/SettingsView.swift
+++ HashCamera/SettingsView.swift
@@ -23,44 +23,33 @@ struct SettingsView: View {
     var body: some View {
         //Photo Format
         
-        VStack {
-            
-            HStack(alignment: .center) {
-                Spacer()
-                Text(localizedString(forKey: "N007_1", value: "Settings"))
-                    .font(.headline)
-                    .padding()
-                Spacer()
-            }
-            .overlay(alignment:.topLeading ) {
-                closeButton
-            }
-            
-            List {
-//                Section {
-//                    Toggle("Location Info", isOn: $locationInfo)
-//                        .onChange(of: locationInfo) { newValue in
-//                            CameraSetting.locationInfo = newValue
-//                        }
-//                    
-//                }
+        NavigationView {
+            VStack {
                 
-                Picker(localizedString(forKey: "N007_2", value: "Photo Format"), selection: $photoFormat) {
-                    ForEach(PhotoFileFormat.allCases) {
-                        Text("\($0.string)")
-                            .tag($0)
+                List {
+ 
+                    Picker(localizedString(forKey: "N007_2", value: "Photo Format"), selection: $photoFormat) {
+                        ForEach(PhotoFileFormat.allCases) {
+                            Text("\($0.string)")
+                                .tag($0)
+                        }
                     }
-                }
-                .pickerStyle(.inline)
-                .onChange(of: photoFormat) { newValue in
-                    CameraSetting.photoFileFormat = newValue
-                }
-                
-                Section {
-                    Button(action: {
-                        EmailHelper.shared
-                            .sendEmail(subject: "Help & Feedback",
-                                       body:
+                    .pickerStyle(.inline)
+                    .onChange(of: photoFormat) { newValue in
+                        CameraSetting.photoFileFormat = newValue
+                    }
+                    
+                    Section {
+                        NavigationLink(destination: WidgetSettingView()) {
+                            Text("Widget Setting")
+                        }
+                    }
+                    
+                    Section {
+                        Button(action: {
+                            EmailHelper.shared
+                                .sendEmail(subject: "Help & Feedback",
+                                           body:
                                    """
                                    [HashCamera]
                                    Version: \(AppStatus.fullVersion)
@@ -68,38 +57,30 @@ struct SettingsView: View {
                                    OS: \(AppStatus.getOsVersion())
                                    
                                    """,
-                                       to: "anto.wg.yang@gmail.com"  )
-                    }, label: {
-                        Text(localizedString(forKey: "N007_3", value: "Email"))
-                    })
-                }
-                
-                Section {
-                    HStack(alignment: .center) {
-                        Text(localizedString(forKey: "N007_4", value: "Version"))
-                        Spacer()
-                        Text(AppStatus.fullVersion)
-                        
+                                           to: "anto.wg.yang@gmail.com"  )
+                        }, label: {
+                            Text(localizedString(forKey: "N007_3", value: "Email"))
+                        })
+                  
+                        HStack(alignment: .center) {
+                            Text(localizedString(forKey: "N007_4", value: "Version"))
+                            Spacer()
+                            Text(AppStatus.fullVersion)
+                            
+                        }
                     }
+                    
                 }
-                
             }
-        }
-        .navigationTitle(localizedString(forKey: "N007_1", value: "Settings"))
+            .navigationTitle(localizedString(forKey: "N007_1", value: "Settings"))
+            .navigationBarItems(leading: Button(action: {
+                presentationMode.wrappedValue.dismiss()
+            }) {
+                Image(uiImage: SystemUIImage.xmark!)
+            })
+        } //./NavigationView
     }
-    
-    private var closeButton: some View {
-        Button {
-            presentationMode.wrappedValue.dismiss()
-        } label: {
-            Image(systemName: "xmark")
-                .font(.headline)
-        }
-        .buttonStyle(.bordered)
-        .clipShape(Circle())
-        .tint(.purple)
-        .padding()
-    }
+
 }
 
 #Preview {
--- HashCamera/WidgetSettingScene/WidgetSettingView.swift
+++ HashCamera/WidgetSettingScene/WidgetSettingView.swift
@@ -15,59 +15,60 @@ struct WidgetSettingView: View {
     @State private var selectedItems: [String] = []
     
     var body: some View {
-        
-        GeometryReader { geometry in
-            
-            VStack {
+            GeometryReader { geometry in
                 
-                Rectangle()
-                    .foregroundStyle(Color.cyan.opacity(0.2))
-                //                    .frame(width: 350, height: 150)
-                    .frame(height:geometry.size.height/4)
-                    .overlay {
-                        FolderListWidgetView(entry: FolderListEntry(
-                            date: Date(),
-                            folderList: vm.selectedItems.map({$0.name})
-                        ))
+                VStack {
+                    
+                    Rectangle()
+                        .foregroundStyle(Color.cyan.opacity(0.2))
+                    //                    .frame(width: 350, height: 150)
+                        .frame(height:geometry.size.height/4)
+                        .overlay {
+                            FolderListWidgetView(entry: FolderListEntry(
+                                date: Date(),
+                                folderList: vm.selectedItems.map({$0.name})
+                            ))
+                            .padding(16)
+                        }
+                        .cornerRadius(16)
                         .padding(16)
-                    }
-                    .cornerRadius(16)
-                    .padding(16)
-                List {
-                    Section(header: Spacer(minLength: 0)) {
-                        
-                        ForEach(vm.folders, id: \.self) { item in
-                            HStack {
-                                Text(item.name)
-                                
-                                Spacer()
-                                
-                                // 선택된 아이템일 경우 별표 표시
-                                if vm.selectedItems.contains(item) {
-                                    Image(systemName: "star.fill")
-                                        .foregroundColor(.yellow)
-                                        .font(.system(size: 16))
+                    List {
+                        Section(header: Spacer(minLength: 0)) {
+                            
+                            ForEach(vm.folders, id: \.self) { item in
+                                HStack {
+                                    Text(item.name)
+                                    
+                                    Spacer()
+                                    
+                                    // 선택된 아이템일 경우 별표 표시
+                                    if vm.selectedItems.contains(item) {
+                                        Image(systemName: "star.fill")
+                                            .foregroundColor(.yellow)
+                                            .font(.system(size: 16))
+                                    }
+                                }
+                                .contentShape(Rectangle()) // 전체 영역 클릭 가능
+                                .onTapGesture {
+                                    vm.toggleSelection(for: item)
                                 }
                             }
-                            .contentShape(Rectangle()) // 전체 영역 클릭 가능
-                            .onTapGesture {
-                                vm.toggleSelection(for: item)
-                            }
                         }
+                        
                     }
-                   
-                }
-                .listStyle(.insetGrouped)
-                .environment(\.defaultMinListHeaderHeight, 0)
-                .onAppear {
+                    .listStyle(.insetGrouped)
+                    .environment(\.defaultMinListHeaderHeight, 0)
+                    .onAppear {
 #if DEBUG
-                    vm.initSampleData()
+                        vm.initSampleData()
 #else
-                    vm.initFolders()
+                        vm.initFolders()
 #endif
+                    }
                 }
             }
-        }
+//            .navigationTitle("Widget Setting")
+            .navigationBarTitle("Widget Setting", displayMode: .inline)
     }
 }
 
